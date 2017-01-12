053d46f8b677764d2a2b22dc7ff15dc1bfb9c99b
Tim <timothyhsieh27@gmail.com>
2017-01-11 21:42:07 -0500

    Updated for student use. Test file is used to clean and check palindromic status of sample text from palindromic_tests.rb.

--- test_functions.rb
+++ test_functions.rb
@@ -1,4 +1,6 @@
 def is_palindrome?(text)
-  text = text.downcase.gsub(/\W/, '')
+  # Line 3 cleans the input text from the user.
+  text = text.downcase
+  # Line 5 checks to see if the cleaned text is the same forwards and backwards.
   text == text.reverse
 end
