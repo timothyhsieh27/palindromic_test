52d4febffcbb951ecc51ce7ce8a9f7d48045f9e6
Tim <timothyhsieh27@gmail.com>
2017-01-11 21:13:24 -0500

    Wrote tests that incrementally verify various instances of palindromes.

--- palindromic_tests.rb
+++ palindromic_tests.rb
@@ -2,4 +2,18 @@ require 'minitest/autorun'
 require_relative 'test_functions'
 #
 class TestPalindromic < Minitest::Test
+  def test_if_palindrome?
+    # basic palindrome
+    assert_equal(TRUE, is_palindrome?('bob'))
+    assert_equal(TRUE, is_palindrome?('272'))
+
+    # palindrome with capitalization
+    assert_equal(TRUE, is_palindrome?('Bob'))
+
+    # palindrome with space
+    assert_equal(TRUE, is_palindrome?('Bo b'))
+
+    # palindrome with punctuation
+    assert_equal(TRUE, is_palindrome?('Bob? BOB!'))
+  end
 end
